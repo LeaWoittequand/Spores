1d0f49ac3647fc7f8092a585c7ae605f5432e8dd
LeaWoittequand <lea.wtd@gmail.com>
2017-09-24 11:43:21 +0200

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :reviews
-
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :description, presence: true
-
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
