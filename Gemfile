3fd642aadbe6aea8a209a88fdadcba9e7647573c
LeaWoittequand <lea.wtd@gmail.com>
2017-09-25 17:24:11 +0200

    first step geocoding : gem + viewflat

--- Gemfile
+++ Gemfile
@@ -17,7 +17,14 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'devise'
 gem 'omniauth-facebook'
+gem "geocoder"
 
+gem "coffee-rails"
+gem "gmaps4rails"
+
+source 'https://rails-assets.org' do
+  gem "rails-assets-underscore"
+end
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,6 @@
 class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def create
   end
 
@@ -9,9 +11,18 @@ class FlatsController < ApplicationController
   end
 
   def index
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 
   def show
+    @flat = Flat.find(params[:id])
+    @flat_coordinates = { lat: @flat.latitude, lng: @flat.longitude }
   end
 
   def new
