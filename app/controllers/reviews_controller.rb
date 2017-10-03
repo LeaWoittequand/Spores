5c7dc83bc70692638216a6bb982a3b91a7c48482
LeaWoittequand <lea.wtd@gmail.com>
2017-10-03 17:09:07 +0200

    button add review and form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:new, :create]
+
   def create
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def index
+    set_review
+    @review = Review.new(review_params)
+    @review.flat = @flat
+    if @review.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def new
+    set_review
+    @review = Review.new
   end
 
-  def update
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :picture)
   end
+
+  def set_review
+    @flat = Flat.find(params[:flat_id])
+  end
+
 end
