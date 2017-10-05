443c4aa1f4581953b5cc55dfee8907665e42bfea
LeaWoittequand <lea.wtd@gmail.com>
2017-10-05 11:29:57 +0200

    fix review + minimum style show + buttonlink

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925150642) do
+ActiveRecord::Schema.define(version: 20171005082056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170925150642) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "flat_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
