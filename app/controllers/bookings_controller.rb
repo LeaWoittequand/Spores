fe17eb0a41d201f33bc29f491d084efd3e3e767c
LeaWoittequand <lea.wtd@gmail.com>
2017-10-30 08:04:39 +0100

    form booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,36 @@
 class BookingsController < ApplicationController
+   before_action :set_booking, only:[:edit, :show]
+
   def create
-  end
+     if booking_params['begin_date'] > booking_params['end_date'] || booking_params['end_date'].blank? ||booking_params['end_date'] < Time.now
+      flash[:alert] = "Dates invalides"
+      return redirect_to flat_path(Flat.find(booking_params[:flat_id]))
+    end
 
-  def destroy
-  end
-
-  def edit
+    user = current_user
+    @booking = user.bookings.new(booking_params)
+    if @booking.save
+      redirect_to booking_path
+    else
+      render :new
+    end
   end
 
   def index
+    @bookings = Booking.all.where(user: current_user)
+    @user = current_user
   end
 
   def new
+    @booking = Booking.new
   end
 
-  def update
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:begin_date, :end_date, :member_of_people)
   end
+
 end
