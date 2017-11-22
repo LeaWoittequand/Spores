55bcf3c96ffd2e1b60d1ffb1a411177060d961a0
LeaWoittequand <lea.wtd@gmail.com>
2017-11-22 15:07:55 +0100

    Lyon + index

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ Flat.create(
   lng: -0.2537897,
   description: ('bla ' * 10),
   picture: 'https://res.cloudinary.com/wagon/image/upload/c_fill,h_200/v1500988849/nqoqw5sinlj1hqvxrfyi.jpg',
-  address: 'Calle de los Conejos, 1',
+  address: 'Cullera',
   city: "Cullera",
   postcode: "46400"
 )
 
 Flat.create(
-  name: "Le Wagon",
-  lat: 50.632668,
-  lng: 3.017586,
+  name: "Morgon",
+  lat: 46.145215,
+  lng: 4.678339,
   description: ('bla ' * 10),
   picture: 'https://res.cloudinary.com/wagon/image/upload/c_fill,h_200/v1500988849/nqoqw5sinlj1hqvxrfyi.jpg',
-  address: '2 avenue des saules',
-  city: "Lille",
-  postcode: "59000"
+  address: 'Morgon',
+  city: "Morgon",
+  postcode: "69910"
 )
