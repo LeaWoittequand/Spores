b968ee464197a15e412570f4d245857bc074a691
LeaWoittequand <lea.wtd@gmail.com>
2017-11-22 21:14:58 +0100

    change jpeg image

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Spores
   class Application < Rails::Application
     config.generators do |generate|
@@ -25,7 +27,7 @@ module Spores
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
