d426adf70d2c2ae7b2883fa82b07a9737bc1f0e9
LeaWoittequand <lea.wtd@gmail.com>
2018-01-04 18:39:33 +0100

    fixcss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
 
-  # Do not compress assets
-  config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
