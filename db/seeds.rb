c3acb6f9a90da7273649750880de99b514982a50
AllanBallester <allan.ballester@decathlon.com>
2018-02-11 22:56:52 +0100

    bye

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,21 +40,3 @@ Flat.create(
   portfolio3: "http://www.telegraph.co.uk/content/dam/Travel/leadAssets/34/92/BeaujolaisNouveau_3492513a-xlarge.jpg",
   portfolio4: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1513882334/bianca-isofache-400477_rayd72.jpg"
 )
-
-Flat.create(
-  name: "Tenerife",
-  lat: 28.390170,
-  lng: -16.499442,
-  description: "That's the beginning of 'Spores expansion'. Many of our users were requesting us that they wanted more Spores Homes but also ready to contribute financially for them.In the other hand Spores did not demonstrate yet that they were financially sustainable as they wanted to encourage social entrepreneurs.Two questions came to our mind : Why 20 peoples can't own an Home ? Why can't we become producers for something linked to local culture ? We learnt laws and build an SCI which allowed Multiple Ownership of French Property. We stimulate our neurons and ends with France and Wine Makers.",
-  environment: "The home is surrounded by wine grasps, you are at the epicenter of the wine culture and french gastronomy. There is also calm roads for road cycling and parcs to hike. There will be 40+ people capacity, from family rooms to huts, tents and hammocks. In the core of a traditional french village. 30 minutes from Lyon by train",
-  status:"This Spores Homes is on Stage 1 : In funding. It means that if you want to be one of the rare owners of Spores Homes discover now without waiting our Co owning plans. Less than 5 seats remaining !!!ome will start from scratch.",
-  baseline: "Welcome to where we make the Spores' Wine .",
-  picture: 'http://proprietes.lefigaro.fr/indicateur/images/PDF/CMS/articles/126165342-1499344234.12.jpg',
-  address: 'La Orotava',
-  city: "La Orotava",
-  postcode: "38310",
-  portfolio1: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1513882532/armando-castillejos-144076_xarrpk.jpg",
-  portfolio2: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1513883125/quino-al-442692_ogqqla.jpg",
-  portfolio3: "http://www.telegraph.co.uk/content/dam/Travel/leadAssets/34/92/BeaujolaisNouveau_3492513a-xlarge.jpg",
-  portfolio4: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1513882334/bianca-isofache-400477_rayd72.jpg"
-)
